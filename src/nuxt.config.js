2daddd80d961c4d8c33fcb2c5b9457b4be302001
shimakaze-git <shimakaze.soft+github@googlemail.com>
2018-06-12 03:35:12 +0000

    first commit

---
+++ src/nuxt.config.js
@@ -0,0 +1,70 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'src',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'nuxt-iot project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  
+  buildDir: '../functions/nuxt',
+  // build: {
+  //   /*
+  //   ** Run ESLint on save
+  //   */
+  //   extend (config, { isDev, isClient }) {
+  //     if (isDev && isClient) {
+  //       config.module.rules.push({
+  //         enforce: 'pre',
+  //         test: /\.(js|vue)$/,
+  //         loader: 'eslint-loader',
+  //         exclude: /(node_modules)/
+  //       })
+  //     }
+  //   }
+  // },
+  build: {
+    publicPath: '/assets/',
+    extractCSS: true,
+    babel: {
+      presets: [
+        'env',
+        'stage-0'
+      ],
+      plugins: [
+        ['transform-runtime', {
+          polyfill: true,
+          regenerator: true
+        }],
+      ],
+    },
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  
+}
